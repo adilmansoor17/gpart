2446b326e6ecfaac4c9b68d559f51f6afd15ef33
Adil Mansoor Ahmad <adil.mansoor@doctornow.io>
2022-10-10 19:19:13 +0500

    update

--- app/src/main/java/com/example/gpark/HomepageActivity.java
+++ app/src/main/java/com/example/gpark/HomepageActivity.java
@@ -1,19 +1,28 @@
 package com.example.gpark;
 
-import android.os.Bundle;
-import android.widget.FrameLayout;
+import static com.google.android.gms.common.util.CollectionUtils.setOf;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 
-public class HomepageActivity extends AppCompatActivity {
+public class HomepageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    FrameLayout frameLayout;
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
@@ -21,17 +30,71 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        toolbar = findViewById(R.id.toolbarr);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         drawerLayout = findViewById(R.id.drawerlayout);
         navigationView = findViewById(R.id.navigation);
-        frameLayout = findViewById(R.id.main_framelayout);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
 
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this,"Click on list", Toast.LENGTH_SHORT);
+
+        switch(item.getItemId()){
+            case R.id.home:
+                Toast.makeText(this,"home", Toast.LENGTH_SHORT);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.navHostFragment,new HomeFragment()).commit();
+                break;
+            case R.id.map:
+                Toast.makeText(this,"map", Toast.LENGTH_SHORT);
+                getSupportFragmentManager().beginTransaction().replace(R.id.navHostFragment,new MapFragment()).commit();
+                break;
+            case R.id.history:
+                Toast.makeText(this,"history", Toast.LENGTH_SHORT);
+                getSupportFragmentManager().beginTransaction().replace(R.id.navHostFragment,new HistoryFragment()).commit();
+                break;
+            case R.id.vip:
+                Toast.makeText(this,"vip", Toast.LENGTH_SHORT);
+                getSupportFragmentManager().beginTransaction().replace(R.id.navHostFragment,new VipFragment()).commit();
+                break;
+            case R.id.users:
+                Toast.makeText(this,"users", Toast.LENGTH_SHORT);
+                getSupportFragmentManager().beginTransaction().replace(R.id.navHostFragment,new UsersFragment()).commit();
+                break;
+            case R.id.detail:
+                Toast.makeText(this,"detail", Toast.LENGTH_SHORT);
+                getSupportFragmentManager().beginTransaction().replace(R.id.navHostFragment,new DetailFragment()).commit();
+                break;
+            case R.id.booking:
+                Toast.makeText(this,"booking", Toast.LENGTH_SHORT);
+                getSupportFragmentManager().beginTransaction().replace(R.id.navHostFragment,new BookingFragment()).commit();
+                break;
+            default: break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
